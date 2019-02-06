053fd228eab7a25ee011f823ffe5994220eaa8fa
Arvids Gargurnis <arvids.gargurnis@plymouth.ac.uk>
2019-02-06 00:05:11 +0000

    aligned top text in how page, added arrow in how page, adjusted font size across website to match

--- js/fit-about.js
+++ js/fit-about.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.l-container p').fitText(3.3, {
+  $('.l-container p').fitText(2.92, {
     minFontSize: '10px',
     maxFontSize: '20px'
   });
--- js/fit-interior.js
+++ js/fit-interior.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.r-container p').fitText(2, {
+  $('.r-container p').fitText(2.26, {
     minFontSize: '10px',
     maxFontSize: '20px'
   });
